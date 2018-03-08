024b0cf2d6d25edd4a9a50aaf41ea701cef26a39
iawia002 <z2d@jifangcheng.com>
2018-03-08 18:25:14 +0800

    extractors/bilibili: split up

--- extractors/bilibili.go
+++ extractors/bilibili.go
@@ -22,47 +22,6 @@ const (
 	secKey string = "94aba54af9065f71de72f5508f1cd42e"
 )
 
-type dURLData struct {
-	Size  int64  `json:"size"`
-	URL   string `json:"url"`
-	Order int    `json:"order"`
-}
-
-type bilibiliData struct {
-	DURL    []dURLData `json:"durl"`
-	Format  string     `json:"format"`
-	Quality int        `json:"quality"`
-}
-
-// {"code":0,"message":"0","ttl":1,"data":{"token":"aaa"}}
-// {"code":-101,"message":"账号未登录","ttl":1}
-type tokenData struct {
-	Token string `json:"token"`
-}
-
-type token struct {
-	Code    int       `json:"code"`
-	Message string    `json:"message"`
-	Data    tokenData `json:"data"`
-}
-
-type bangumiEpData struct {
-	EpID int `json:"ep_id"`
-}
-
-type bangumiData struct {
-	EpList []bangumiEpData `json:"epList"`
-}
-
-var quality = map[int]string{
-	116: "高清 1080P60",
-	74:  "高清 720P60",
-	80:  "高清 1080P",
-	64:  "高清 720P",
-	32:  "清晰 480P",
-	15:  "流畅 360P",
-}
-
 func getSign(params string) string {
 	sign := md5.New()
 	sign.Write([]byte(params + secKey))
