73da865b7df1380e2ee2103b57634ccfdd1342aa
iawia002 <z2d@jifangcheng.com>
2018-03-20 14:39:24 +0800

    main: support FreeBSD fix #25 [ci skip]

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ from fabric.api import (
 )
 
 
-def build(args='-os="linux darwin windows" -arch="386 amd64"'):
+def build(args='-os="linux darwin windows freebsd" -arch="386 amd64"'):
     local(
         'gox {}'.format(args)
     )
