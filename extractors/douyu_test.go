b348c5741ba2ac9c704d691d5798b0c715c4005f
Jin Liu <coolziljin@gmail.com>
2018-03-28 15:20:10 +0800

    extractors/douyu: Add basic support

---
+++ extractors/douyu_test.go
@@ -0,0 +1,31 @@
+package extractors
+
+import (
+	"testing"
+
+	"github.com/iawia002/annie/config"
+	"github.com/iawia002/annie/test"
+)
+
+func TestDouyu(t *testing.T) {
+	config.InfoOnly = true
+	tests := []struct {
+		name string
+		args test.Args
+	}{
+		{
+			name: "normal test",
+			args: test.Args{
+				URL:   "https://v.douyu.com/show/l0Q8mMY3wZqv49Ad",
+				Title: "每日撸报_每日撸报：有些人死了其实它还可以把你带走_斗鱼视频 - 最6的弹幕视频网站",
+				Size:  10558080,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			data := Douyu(tt.args.URL)
+			test.Check(t, tt.args, data)
+		})
+	}
+}
