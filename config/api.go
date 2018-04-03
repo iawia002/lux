efb8ce07ae99458150d9849ca75e2e615290ba1a
iawia002 <z2d@jifangcheng.com>
2018-04-03 14:53:54 +0800

    extractors/bilibili: 🔧

--- config/api.go
+++
@@ -1,8 +0,0 @@
-package config
-
-// Bilibili
-const (
-	BILIBILI_API         string = "https://interface.bilibili.com/v2/playurl?"
-	BILIBILI_BANGUMI_API string = "https://bangumi.bilibili.com/player/web_api/v2/playurl?"
-	BILIBILI_TOKEN_API   string = "https://api.bilibili.com/x/player/playurl/token?"
-)
