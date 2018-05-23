5027223d5bb7e4adc645b91fd197f572fc23a7c9
iawia002 <z2d@jifangcheng.com>
2018-05-23 22:12:27 +0800

    extractors/douyin: 🔧

--- extractors/douyin.go
+++ extractors/douyin.go
@@ -1,7 +1,8 @@
 package extractors
 
 import (
-	"encoding/json"
+	// "encoding/json"
+	"strings"
 
 	"github.com/iawia002/annie/downloader"
 	"github.com/iawia002/annie/request"
@@ -24,11 +25,9 @@ type douyinData struct {
 // Douyin download function
 func Douyin(url string) downloader.VideoData {
 	html := request.Get(url, url, nil)
-	vData := utils.MatchOneOf(html, `var data = \[(.*?)\];`)[1]
-	var dataDict douyinData
-	json.Unmarshal([]byte(vData), &dataDict)
-
-	realURL := dataDict.Video.PlayAddr.URLList[0]
+	title := utils.MatchOneOf(html, `<p class="desc">(.+?)</p>`)[1]
+	realURL := utils.MatchOneOf(html, `playAddr: "(.+?)"`)[1]
+	realURL = strings.Replace(realURL, "/playwm/", "/play/", 1)
 	size := request.Size(realURL, url)
 	urlData := downloader.URLData{
 		URL:  realURL,
@@ -43,7 +42,7 @@ func Douyin(url string) downloader.VideoData {
 	}
 	extractedData := downloader.VideoData{
 		Site:    "抖音 douyin.com",
-		Title:   utils.FileName(dataDict.Desc),
+		Title:   utils.FileName(title),
 		Type:    "video",
 		Formats: format,
 	}
