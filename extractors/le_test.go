a948594e39c635c4f7c72b37e241885fb8feabe5
iawia002 <z2d@jifangcheng.com>
2018-07-31 15:20:40 +0800

    tests/le: ⬆️

--- extractors/le_test.go
+++ extractors/le_test.go
@@ -18,7 +18,6 @@ func TestLe(t *testing.T) {
 			args: test.Args{
 				URL:     "http://www.le.com/ptv/vplay/31453073.html",
 				Title:   "难以管理！中国共享单车撤出华盛顿",
-				Size:    7006196,
 				Quality: "高清 960x544",
 			},
 		},
@@ -27,7 +26,6 @@ func TestLe(t *testing.T) {
 			args: test.Args{
 				URL:     "http://www.le.com/ptv/vplay/31448498.html",
 				Title:   "天行九歌 60",
-				Size:    398149972,
 				Quality: "1080P 1920x1072",
 			},
 		},
