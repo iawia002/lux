9600b683f96d9b2970114aeeaba1da80f03f762d
iawia002 <xuxinzhao@caicloud.io>
2020-05-09 13:02:18 +0800

    douyin: refactor

--- extractors/douyin/douyin.go
+++ extractors/douyin/douyin.go
@@ -1,10 +1,20 @@
 package douyin
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
 	"github.com/iawia002/annie/extractors/types"
 	"github.com/iawia002/annie/request"
 	"github.com/iawia002/annie/utils"
 )
+
+type data struct {
+	ItemList []struct {
+		Desc string `json:"desc"`
+	} `json:"item_list"`
+}
 
 type extractor struct{}
 
@@ -20,13 +30,7 @@ func (e *extractor) Extract(url string, option types.Options) ([]*types.Data, er
 	if err != nil {
 		return nil, err
 	}
-	var title string
-	desc := utils.MatchOneOf(html, `<p class="desc">(.+?)</p>`)
-	if desc != nil {
-		title = desc[1]
-	} else {
-		title = "抖音短视频"
-	}
+
 	realURLs := utils.MatchOneOf(html, `playAddr: "(.+?)"`)
 	if realURLs == nil || len(realURLs) < 2 {
 		return nil, types.ErrURLParseFailed
@@ -48,10 +52,36 @@ func (e *extractor) Extract(url string, option types.Options) ([]*types.Data, er
 			Size:  size,
 		},
 	}
+
+	videoIDs := utils.MatchOneOf(url, `/video/(\d+)`)
+	if len(videoIDs) == 0 {
+		return nil, errors.New("unable to get video ID")
+	}
+	videoID := videoIDs[1]
+
+	dytks := utils.MatchOneOf(html, `dytk: "(.+?)"`)
+	if len(dytks) == 0 {
+		return nil, errors.New("unable to get dytk info")
+	}
+	dytk := dytks[1]
+
+	apiDataString, err := request.Get(
+		fmt.Sprintf("https://www.douyin.com/web/api/v2/aweme/iteminfo/?item_ids=%s&dytk=%s", videoID, dytk),
+		url, nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var apiData data
+	if err = json.Unmarshal([]byte(apiDataString), &apiData); err != nil {
+		return nil, err
+	}
+
 	return []*types.Data{
 		{
 			Site:    "抖音 douyin.com",
-			Title:   title,
+			Title:   apiData.ItemList[0].Desc,
 			Type:    types.DataTypeVideo,
 			Streams: streams,
 			URL:     url,
