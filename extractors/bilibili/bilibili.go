iawia002/lux

commit e463c203f9956c14a6230e513af1cea0e0598bb1

fix(extractors/bilibili): Change the parsing method for some videos

extractors/bilibili/bilibili.go
@@ -74,25 +74,40 @@ func genAPI(aid, cid, quality int, bangumi bool, cookie string) (string, error) 
 }
 
 func genParts(dashData *dashInfo, quality int, referer string) ([]*types.Part, error) {
-	parts := make([]*types.Part, 2)
-	checked := false
-	for _, stream := range dashData.Streams.Video {
-		if stream.ID == quality {
-			s, err := request.Size(stream.BaseURL, referer)
-			if err != nil {
-				return nil, err
-			}
-			parts[0] = &types.Part{
-				URL:  stream.BaseURL,
-				Size: s,
-				Ext:  "mp4",
-			}
-			checked = true
-			break
+	parts := make([]*types.Part, 1)
+	if dashData.Streams.Audio == nil {
+		url := dashData.DURL[0].URL
+		_, ext, err := utils.GetNameAndExt(url)
+		if err != nil {
+			return nil, err
 		}
-	}
-	if !checked {
-		return nil, nil
+		parts[0] = &types.Part{
+			URL:  url,
+			Size: dashData.DURL[0].Size,
+			Ext:  ext,
+		}
+
+	} else {
+
+		checked := false
+		for _, stream := range dashData.Streams.Video {
+			if stream.ID == quality {
+				s, err := request.Size(stream.BaseURL, referer)
+				if err != nil {
+					return nil, err
+				}
+				parts[0] = &types.Part{
+					URL:  stream.BaseURL,
+					Size: s,
+					Ext:  "mp4",
+				}
+				checked = true
+				break
+			}
+		}
+		if !checked {
+			return nil, nil
+		}
 	}
 	return parts, nil
 }
@@ -308,25 +323,28 @@ func bilibiliDownload(options bilibiliOptions, extractOption types.Options) *typ
 		dashData = data.Data
 	}
 
-	// Get audio part
-	var audioID int
-	audios := map[int]string{}
-	bandwidth := 0
-	for _, stream := range dashData.Streams.Audio {
-		if stream.Bandwidth > bandwidth {
-			audioID = stream.ID
+	var audioPart *types.Part
+	if dashData.Streams.Audio != nil {
+		// Get audio part
+		var audioID int
+		audios := map[int]string{}
+		bandwidth := 0
+		for _, stream := range dashData.Streams.Audio {
+			if stream.Bandwidth > bandwidth {
+				audioID = stream.ID
+			}
+			audios[stream.ID] = stream.BaseURL
+			bandwidth = stream.Bandwidth
 		}
-		audios[stream.ID] = stream.BaseURL
-		bandwidth = stream.Bandwidth
-	}
-	s, err := request.Size(audios[audioID], referer)
-	if err != nil {
-		return types.EmptyData(options.url, err)
-	}
-	audioPart := &types.Part{
-		URL:  audios[audioID],
-		Size: s,
-		Ext:  "m4a",
+		s, err := request.Size(audios[audioID], referer)
+		if err != nil {
+			return types.EmptyData(options.url, err)
+		}
+		audioPart = &types.Part{
+			URL:  audios[audioID],
+			Size: s,
+			Ext:  "m4a",
+		}
 	}
 
 	streams := make(map[string]*types.Stream, len(dashData.Quality))
@@ -360,15 +378,20 @@ func bilibiliDownload(options bilibiliOptions, extractOption types.Options) *typ
 		if err != nil {
 			return types.EmptyData(options.url, err)
 		}
-		parts[1] = audioPart
 		var size int64
 		for _, part := range parts {
 			size += part.Size
+		}
+		if audioPart != nil {
+			parts = append(parts, audioPart)
 		}
 		streams[strconv.Itoa(q)] = &types.Stream{
 			Parts:   parts,
 			Size:    size,
 			Quality: qualityString[q],
+		}
+		if audioPart != nil {
+			streams[strconv.Itoa(q)].NeedMux = true
 		}
 	}
 

extractors/bilibili/bilibili_test.go
@@ -27,7 +27,7 @@ func TestBilibili(t *testing.T) {
 			args: test.Args{
 				URL:     "https://www.bilibili.com/video/av41301960",
 				Title:   "【英雄联盟】2019赛季CG 《觉醒》",
-				Size:    65774670,
+				Size:    62266048,
 				Quality: "高清 1080P",
 			},
 			playlist: false,

downloader/downloader.go
@@ -634,7 +634,7 @@ func (downloader *Downloader) Download(data *types.Data) error {
 	}
 
 	fmt.Printf("Merging video parts into %s\n", mergedFilePath)
-	if stream.Ext != "mp4" || data.Site == "YouTube youtube.com" || data.Site == "哔哩哔哩 bilibili.com" {
+	if stream.Ext != "mp4" || stream.NeedMux == true {
 		return utils.MergeFilesWithSameExtension(parts, mergedFilePath)
 	}
 	return utils.MergeToMP4(parts, mergedFilePath, title)

extractors/bilibili/types.go
@@ -51,11 +51,17 @@ type dashStreams struct {
 	Audio []dashStream `json:"audio"`
 }
 
+type dURL struct {
+	Size int64  `json:"size"`
+	URL  string `json:"url"`
+}
+
 type dashInfo struct {
 	CurQuality  int         `json:"quality"`
 	Description []string    `json:"accept_description"`
 	Quality     []int       `json:"accept_quality"`
 	Streams     dashStreams `json:"dash"`
+	DURL        []dURL      `json:"durl"`
 }
 
 type dash struct {

extractors/types/types.go
@@ -21,6 +21,8 @@ type Stream struct {
 	Size int64 `json:"size"`
 	// the file extension after video parts merged
 	Ext string `json:"ext"`
+	// if the parts need mux
+	NeedMux bool
 }
 
 // DataType indicates the type of extracted data, eg: video or image.

extractors/youtube/youtube.go
@@ -228,6 +228,7 @@ func genStream(videoFormat *streamFormat, videoInfo *ytdl.VideoInfo) (*types.Str
 		ID:      strconv.Itoa(videoFormat.Itag),
 		Parts:   []*types.Part{video},
 		Quality: quality,
+		NeedMux: true,
 	}, nil
 }
 

commit b1b3fb83de74451b8fa4775f958ff0657edd4815

extractors/bilibili: fix low quality audio issue #790 and incorrect muxed video size info bug * fix low quality audio issue #790 * fix incorrect muxed video size info bug introduced in e463c203f9956c14a6230e513af1cea0e0598bb1

extractors/bilibili/bilibili.go
@@ -276,7 +276,7 @@ func (e *extractor) Extract(url string, option types.Options) ([]*types.Data, er
 		return nil, err
 	}
 
-	// set thread number to 1 manually to avoid http 412 error 
+	// set thread number to 1 manually to avoid http 412 error
 	option.ThreadNumber = 1
 	fmt.Printf("Warning: Multi thread download is no longer supported by BiliBili, use single thread instead.\n")
 
@@ -337,9 +337,9 @@ func bilibiliDownload(options bilibiliOptions, extractOption types.Options) *typ
 		for _, stream := range dashData.Streams.Audio {
 			if stream.Bandwidth > bandwidth {
 				audioID = stream.ID
+				bandwidth = stream.Bandwidth
 			}
 			audios[stream.ID] = stream.BaseURL
-			bandwidth = stream.Bandwidth
 		}
 		s, err := request.Size(audios[audioID], referer)
 		if err != nil {
@@ -383,12 +383,12 @@ func bilibiliDownload(options bilibiliOptions, extractOption types.Options) *typ
 		if err != nil {
 			return types.EmptyData(options.url, err)
 		}
+		if audioPart != nil {
+			parts = append(parts, audioPart)
+		}
 		var size int64
 		for _, part := range parts {
 			size += part.Size
-		}
-		if audioPart != nil {
-			parts = append(parts, audioPart)
 		}
 		streams[strconv.Itoa(q)] = &types.Stream{
 			Parts:   parts,

extractors/bilibili/bilibili_test.go
@@ -27,7 +27,7 @@ func TestBilibili(t *testing.T) {
 			args: test.Args{
 				URL:     "https://www.bilibili.com/video/av41301960",
 				Title:   "【英雄联盟】2019赛季CG 《觉醒》",
-				Size:    62266048,
+				Size:    70696896,
 				Quality: "高清 1080P",
 			},
 			playlist: false,