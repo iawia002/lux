d72c713f1e93b5c008ae1ce35b89f0a73851819f
iawia002 <z2d@jifangcheng.com>
2021-09-05 10:16:50 +0800

    extractors/bilibili: adopt the new API

--- extractors/bilibili/bilibili_test.go
+++ extractors/bilibili/bilibili_test.go
@@ -16,19 +16,17 @@ func TestBilibili(t *testing.T) {
 		{
 			name: "normal test 1",
 			args: test.Args{
-				URL:     "https://www.bilibili.com/video/av20203945/",
-				Title:   "【2018拜年祭单品】相遇day by day",
-				Quality: "高清 1080P",
+				URL:   "https://www.bilibili.com/video/av20203945/",
+				Title: "【2018拜年祭单品】相遇day by day",
 			},
 			playlist: false,
 		},
 		{
 			name: "normal test 2",
 			args: test.Args{
-				URL:     "https://www.bilibili.com/video/av41301960",
-				Title:   "【英雄联盟】2019赛季CG 《觉醒》",
-				Size:    70696896,
-				Quality: "高清 1080P",
+				URL:   "https://www.bilibili.com/video/av41301960",
+				Title: "【英雄联盟】2019赛季CG 《觉醒》",
+				Size:  70696896,
 			},
 			playlist: false,
 		},
@@ -37,24 +35,21 @@ func TestBilibili(t *testing.T) {
 			args: test.Args{
 				URL:   "https://www.bilibili.com/bangumi/play/ep167000",
 				Title: "狐妖小红娘：第70话 苏苏智商上线",
-				// Quality: "高清 1080P",
 			},
 		},
 		{
 			name: "bangumi playlist test",
 			args: test.Args{
-				URL:     "https://www.bilibili.com/bangumi/play/ss5050",
-				Title:   "一人之下：第1话 异人刀兵起，道炁携阴阳",
-				Quality: "高清 720P",
+				URL:   "https://www.bilibili.com/bangumi/play/ss5050",
+				Title: "一人之下：第1话 异人刀兵起，道炁携阴阳",
 			},
 			playlist: true,
 		},
 		{
 			name: "playlist test",
 			args: test.Args{
-				URL:     "https://www.bilibili.com/video/av16907446/",
-				Title:   "\"不要相信歌词，他们为了押韵什么都干得出来\"",
-				Quality: "高清 720P",
+				URL:   "https://www.bilibili.com/video/av16907446/",
+				Title: "\"不要相信歌词，他们为了押韵什么都干得出来\"",
 			},
 			playlist: true,
 		},
