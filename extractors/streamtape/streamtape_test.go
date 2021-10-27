4cb053477a54889a62c6f61ea362da7a47298bbb
darmiel <71837281+darmiel@users.noreply.github.com>
2021-10-27 15:04:41 +0200

    test: use `test` to check extracted data

--- extractors/streamtape/streamtape_test.go
+++ extractors/streamtape/streamtape_test.go
@@ -23,17 +23,8 @@ func TestStreamtape(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			data, err := New().Extract(tt.args.URL, types.Options{})
-			if err != nil {
-				t.Error(err)
-				return
-			}
-			if len(data) == 0 {
-				t.Error("extractor returned empty data")
-				return
-			}
-			if data[0].Title != tt.args.Title {
-				t.Errorf("expected title '%s' got '%s'", tt.args.Title, data[0].Title)
-			}
+			test.CheckError(t, err)
+			test.Check(t, tt.args, data[0])
 		})
 	}
 }
