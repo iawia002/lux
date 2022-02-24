274bdeb1c65030f657af4f1e78daa13cdb60c9e3
Axetroy <axetroy.dev@gmail.com>
2022-02-24 22:33:15 +0800

    update

--- extractors/kuaishou/kuaishou.go
+++ extractors/kuaishou/kuaishou.go
@@ -1,13 +1,8 @@
 package kuaishou
 
 import (
-	"compress/flate"
-	"compress/gzip"
 	"errors"
-	"io"
-	"io/ioutil"
 	"net/http"
-	stdUrl "net/url"
 	"regexp"
 	"strings"
 
@@ -27,51 +22,14 @@ func New() extractors.Extractor {
 	return &extractor{}
 }
 
-func requestHTML(url string, retryCount int, headers map[string]string) (htmlStr string, cookie string, err error) {
-	if headers == nil {
-		headers = make(map[string]string)
-	}
-
-	if retryCount > 1 {
-		return "", "", errors.New("retry timeout")
-	}
-
-	u := url
-	if retryCount == 0 {
-		u = "https://www.kuaishou.com"
-	}
-
-	res, err := request.Request(http.MethodGet, u, nil, headers)
+// fetch url and get the cookie that write by server
+func fetchCookies(url string, headers map[string]string) (string, error) {
+	res, err := request.Request(http.MethodGet, url, nil, headers)
 	if err != nil {
-		return "", "", err
+		return "", err
 	}
 
 	defer res.Body.Close() // nolint
-
-	var reader io.ReadCloser
-	switch res.Header.Get("Content-Encoding") {
-	case "gzip":
-		reader, _ = gzip.NewReader(res.Body)
-	case "deflate":
-		reader = flate.NewReader(res.Body)
-	default:
-		reader = res.Body
-	}
-	defer reader.Close() // nolint
-
-	b, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return "", "", err
-	}
-
-	htmlStr = string(b)
-
-	titles := utils.MatchOneOf(htmlStr, `<title>([^<]+)</title>`)
-	if titles == nil || len(titles) < 2 {
-		return "", "", extractors.ErrURLParseFailed
-	}
-
-	title := strings.TrimSpace(titles[1])
 
 	cookiesArr := make([]string, 0)
 	cookies := res.Cookies()
@@ -80,32 +38,25 @@ func requestHTML(url string, retryCount int, headers map[string]string) (htmlStr
 		cookiesArr = append(cookiesArr, c.Name+"="+c.Value)
 	}
 
-	cookieStr := strings.Join(cookiesArr, "; ")
-
-	if title == "短视频-快手" || strings.Contains(title, "快手短视频") {
-		retryCount += 1
-		// headers["Cookie"] = "kpf=PC_WEB; kpn=KUAISHOU_VISION; clientid=3; did=web_3931f3e6038ba69679184dcbb2e9c2dc"
-		headers["Cookie"] = cookieStr
-		return requestHTML(url, retryCount, headers)
-	}
-
-	return htmlStr, cookieStr, nil
+	return strings.Join(cookiesArr, "; "), nil
 }
 
 // Extract is the main function to extract the data.
 func (e *extractor) Extract(url string, option extractors.Options) ([]*extractors.Data, error) {
 	var err error
 
-	inputUrl, err := stdUrl.Parse(url)
+	headers := map[string]string{
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0",
+	}
+
+	cookies, err := fetchCookies(url, headers)
 	if err != nil {
 		return nil, err
 	}
 
-	html, _, err := requestHTML(url, 0, map[string]string{
-		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0",
-		"Origin":     inputUrl.Scheme + "://" + inputUrl.Host, // https://github.com
-		"Host":       inputUrl.Hostname(),                     // github.com
-	})
+	headers["Cookie"] = cookies
+
+	html, err := request.Get(url, url, headers)
 	if err != nil {
 		return nil, err
 	}
