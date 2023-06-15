b24e50a82dc5b777df4e8bcdf9ef34c74e6a7f73
shavit <shavit@users.noreply.github.com>
2023-06-15 12:23:00 -0600

    Extract images and videos

--- extractors/instagram/instagram.go
+++ extractors/instagram/instagram.go
@@ -10,50 +10,37 @@ import (
 
 	"github.com/iawia002/lux/extractors"
 	"github.com/iawia002/lux/request"
+	"github.com/iawia002/lux/utils"
 )
 
 func init() {
 	extractors.Register("instagram", New())
 }
 
-type instagram struct {
-	ShortcodeMedia struct {
-		EdgeSidecar struct {
-			Edges []struct {
-				Node struct {
-					DisplayURL string `json:"display_url"`
-					IsVideo    bool   `json:"is_video"`
-					VideoURL   string `json:"video_url"`
-				} `json:"node"`
-			} `json:"edges"`
-		} `json:"edge_sidecar_to_children"`
-	} `json:"shortcode_media"`
-}
-
 type instagramPayload struct {
-    ArticleBody string `json:"articleBody"`
-    Author struct {
-        Image string `json:"image"`
-        Name string `json:"name"`
-        AlternativeName string `json:"alternativeName"`
-        Url string `json:"url"`
-    }`json:"author"`
-    Videos []struct {
-        UploadData string `json:"string"`
-        Description string `json:"description"`
-        Name string `json:"name"`
-        Caption string `json:"caption"`
-        Height string `json:"height"`
-        Width string `json:"width"`
-        ContentURL string `json:"contentUrl"`
-        ThumbnailURL string `json:"thumbnailUrl"`
-    } `json:"video"`
-    Images []struct {
-        Caption string `json:"caption"`
-        Height string `json:"height"`
-        Width string `json:"width"`
-        Url string `json:"url"`
-    } `json:"image"`
+	ArticleBody string `json:"articleBody"`
+	Author      struct {
+		Image           string `json:"image"`
+		Name            string `json:"name"`
+		AlternativeName string `json:"alternativeName"`
+		Url             string `json:"url"`
+	} `json:"author"`
+	Videos []struct {
+		UploadData   string `json:"string"`
+		Description  string `json:"description"`
+		Name         string `json:"name"`
+		Caption      string `json:"caption"`
+		Height       string `json:"height"`
+		Width        string `json:"width"`
+		ContentURL   string `json:"contentUrl"`
+		ThumbnailURL string `json:"thumbnailUrl"`
+	} `json:"video"`
+	Images []struct {
+		Caption string `json:"caption"`
+		Height  string `json:"height"`
+		Width   string `json:"width"`
+		URL     string `json:"url"`
+	} `json:"image"`
 }
 
 type extractor struct{}
@@ -75,55 +62,53 @@ func (e *extractor) Extract(url string, option extractors.Options) ([]*extractor
 		return nil, errors.WithStack(err)
 	}
 
-    reader := strings.NewReader(htmlResp)
-    htmlRoot, err := html.Parse(reader)
-    if err != nil {
-        return nil, errors.WithStack(err)
-    }
+	reader := strings.NewReader(htmlResp)
+	htmlRoot, err := html.Parse(reader)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
 
-    sNode, err := dfsFindScript(htmlRoot)
-    if err != nil {
-        return nil, errors.WithStack(err)
-    }
+	sNode, err := dfsFindScript(htmlRoot)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
 
-    var payload instagramPayload
-    if err = json.Unmarshal([]byte(sNode.Data), &payload); err != nil {
-        return nil, errors.WithStack(err)
-    }
+	var payload instagramPayload
+	if err = json.Unmarshal([]byte(sNode.Data), &payload); err != nil {
+		return nil, errors.WithStack(err)
+	}
 
-    var parts []*extractors.Part
-    if len(payload.Videos) > 0 {
-        for _, it := range payload.Videos {
-            ext := ""
-            part := &extractors.Part{
-                URL:  it.ContentURL,
-                Size: 0,
-                Ext:  ext,
-            }
-            parts = append(parts, part)
-        }
-    } else if len(payload.Images) > 0 {
-        for _, it := range payload.Videos {
-            ext := ""
-            part := &extractors.Part{
-                URL:  it.ContentURL,
-                Size: 0,
-                Ext:  ext,
-            }
-            parts = append(parts, part)
-        }
-    } else {
-        return nil, errors.WithStack(extractors.ErrBodyParseFailed)
-    }
-            
-    streams :=  map[string]*extractors.Stream{
-        "default": {
-            Parts: parts,
-            Size:  0,
-        },
-    };
-    
-    id := u.Path[strings.LastIndex(u.Path, "/") + 1:]
+	var totalSize int64
+	var parts []*extractors.Part
+	if len(payload.Videos) > 0 {
+		videoParts, err := createPartVideos(&payload, url)
+		if err != nil {
+			return nil, errors.WithStack(extractors.ErrBodyParseFailed)
+		}
+
+		parts = append(parts, videoParts...)
+	}
+	if len(payload.Images) > 0 {
+		imageParts, err := createPartImages(&payload, url)
+		if err != nil {
+			return nil, errors.WithStack(extractors.ErrBodyParseFailed)
+		}
+
+		parts = append(parts, imageParts...)
+	}
+
+	for _, part := range parts {
+		totalSize += part.Size
+	}
+
+	streams := map[string]*extractors.Stream{
+		"default": {
+			Parts: parts,
+			Size:  totalSize,
+		},
+	}
+
+	id := u.Path[strings.LastIndex(u.Path, "/")+1:]
 
 	return []*extractors.Data{
 		{
@@ -137,19 +122,63 @@ func (e *extractor) Extract(url string, option extractors.Options) ([]*extractor
 }
 
 func dfsFindScript(n *html.Node) (*html.Node, error) {
-    if n.Type == html.ElementNode && n.Data == "script" {
-        for _, attr := range n.Attr {
-            if attr.Key == "type" && attr.Val == "application/ld+json" {
-                return n.FirstChild, nil
-            }
-        }
-    }
+	if n.Type == html.ElementNode && n.Data == "script" {
+		for _, attr := range n.Attr {
+			if attr.Key == "type" && attr.Val == "application/ld+json" {
+				return n.FirstChild, nil
+			}
+		}
+	}
 
-    for c := n.FirstChild; c != nil; c = c.NextSibling {
-        if ret, err := dfsFindScript(c); err == nil {
-            return ret, nil
-        }
-    }
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if ret, err := dfsFindScript(c); err == nil {
+			return ret, nil
+		}
+	}
 
-    return nil, errors.WithStack(extractors.ErrBodyParseFailed)
+	return nil, errors.WithStack(extractors.ErrBodyParseFailed)
+}
+
+func createPartVideos(payload *instagramPayload, ref string) (parts []*extractors.Part, err error) {
+	for _, it := range payload.Videos {
+		_, ext, err := utils.GetNameAndExt(it.ContentURL)
+		if err != nil {
+			return parts, errors.WithStack(err)
+		}
+		filesize, err := request.Size(it.ContentURL, ref)
+		if err != nil {
+			return parts, errors.WithStack(err)
+		}
+
+		part := &extractors.Part{
+			URL:  it.ContentURL,
+			Size: filesize,
+			Ext:  ext,
+		}
+		parts = append(parts, part)
+	}
+
+	return parts, err
+}
+
+func createPartImages(payload *instagramPayload, ref string) (parts []*extractors.Part, err error) {
+	for _, it := range payload.Images {
+		_, ext, err := utils.GetNameAndExt(it.URL)
+		if err != nil {
+			return parts, errors.WithStack(err)
+		}
+		filesize, err := request.Size(it.URL, ref)
+		if err != nil {
+			return parts, errors.WithStack(err)
+		}
+
+		part := &extractors.Part{
+			URL:  it.URL,
+			Size: filesize,
+			Ext:  ext,
+		}
+		parts = append(parts, part)
+	}
+
+	return parts, err
 }
